f652f8970b038536e3b24524baab4406da2f11e0
orianajpp <orianajpp@gmail.com>
2023-04-25 20:56:07 -0400

    Cambio de error de ventanas emergentes

--- src/index.js
+++ src/index.js
@@ -1,41 +1,63 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 // Obtención de los elementos del DOM
-const abrir = document.getElementById("abrir"); 
+const abrir = document.getElementById("abrir");
+const presentacion = document.getElementById("presentacion");
+const comprar = document.getElementById("comprar");
+const carritoCompras = document.getElementById("carritoCompras");
+const botonPagar = document.getElementById("botonPagar");
 const formularioDePago = document.getElementById("formularioDePago");
-const contenido = document.getElementById("contenido")    
+const contenido = document.getElementById("contenido");
 const pagar = document.getElementById("pagar");
 const respuesta = document.getElementById("respuesta");
-const obtenHeader = document.getElementById('superior');
-const obtenFooter = document.getElementById('pie');
+
+const obtenHeader = document.getElementById("superior");
+const obtenFooter = document.getElementById("pie");
 
 //Ejecución deeEventos del DOM
 abrir.addEventListener("click", () => {
-  formularioDePago.classList.add("prendeFormulario") + contenido.classList.add("frutas") + obtenFooter.classList.add("frutas") + obtenHeader.classList.add("frutas");
+  presentacion.classList.add("prende-presentacion") +
+    contenido.classList.add("frutas") +
+    obtenFooter.classList.add("frutas") +
+    obtenHeader.classList.add("frutas");
 });
 
+comprar.addEventListener("click", () => {
+  carritoCompras.classList.add("prendeCarritoCompras") +
+    presentacion.classList.add("frutas") +
+    contenido.classList.add("frutas") +
+    obtenFooter.classList.add("frutas") +
+    obtenHeader.classList.add("frutas");
+});
+
+botonPagar.addEventListener("click", () => {
+  formularioDePago.classList.add("prendeFormulario") +
+    presentacion.classList.add("frutas") +
+    contenido.classList.add("frutas") +
+    obtenFooter.classList.add("frutas") +
+    obtenHeader.classList.add("frutas");
+});
+/*pagar.addEventListener("click", () => {
+  respuesta.classList.add("prendeRespuesta") + presentacion.classList.add("frutas") + contenido.classList.add("frutas") + obtenFooter.classList.add("frutas") + obtenHeader.classList.add("frutas");
+});*/
 pagar.addEventListener("click", () => {
+  
   const obtenNumero = document.getElementById("tdcNumber").value;
-  const tarjetaValida = validator.isValid(obtenNumero); 
+  const tarjetaValida = validator.isValid(obtenNumero);
   const tarjetaMascara = validator.maskify(obtenNumero);
+  /*respuesta.classList.add("prendeRespuesta") + presentacion.classList.add("frutas") + contenido.classList.add("frutas") + obtenFooter.classList.add("frutas") + obtenHeader.classList.add("frutas");*/
 
-  if (obtenNumero === ''){
-    respuesta.textContent = "Debe ingresar el número de la tarjeta"
-    return
+  if (obtenNumero === "") {
+    respuesta.textContent = "Debe ingresar el número de la tarjeta";
+    return;
+  } else if (obtenNumero.length < 10) {
+    respuesta.textContent = "El número ingresado es incorrecto";
+    return;
   }
-  
-  else if (obtenNumero.length < 10){
-    respuesta.textContent = "El número ingresado es incorrecto"
-    return
-  }
-      
-  
-  if(tarjetaValida === true){
+
+  if (tarjetaValida === true) {
     respuesta.textContent = "tu tarjeta " + tarjetaMascara + " es válida";
-  }
-
-  else{
+  } else {
     respuesta.textContent = "La tarjeta " + tarjetaMascara + " es inválida";
   }
-  
 });
